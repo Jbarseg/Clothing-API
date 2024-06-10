372d07cad4554777cfe2a0b3e8b406641743ca50
Jonathan D. Barrantes Segovia <jhonnyd08@gmail.com>
2024-06-09 20:56:27 -0600

    Using Three-Layer Architecture Pattern

---
+++ src/main/java/com/boutique/momentos/MomentosBoutiqueApplication.java
@@ -0,0 +1,13 @@
+package com.boutique.momentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentosBoutiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentosBoutiqueApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/boutique/momentos/domain/domainentity/PurchaseDomain.java
@@ -0,0 +1,5 @@
+package main.java.com.boutique.momentos.domain.domainentity;
+
+public class PurchaseDomain {
+    
+}
